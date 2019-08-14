d93a95062c74870598833f39530314d7ed3c944b
ProjetoSerraJr <projetoscomputacao@serrajr.eng.br>
2019-08-14 10:48:49 -0300

    MarcoAraujoNeves: Atualização com os métodos utilizados em cada rota.

--- api/routes.php
+++ api/routes.php
@@ -1,49 +1,49 @@
 /*
 ::::EMPRESAS:::::::::
-    /empresa/new body{ "nome":"...", "cnpj":"...", "telefone":"...", "tipoPgto":"..." }
-    /empresa/read header{ "_id":"..." }
-    /empresa/delete header{ "_id":"..." }
-    /empresa/update body{ "_id":"...", "nome":"...", "cnpj":"...", "telefone":"...", "tipoPgto":"..." }
+    POST /empresa/new body{ "nome":"...", "cnpj":"...", "telefone":"...", "tipoPgto":"..." }
+    GET /empresa/read header{ "_id":"..." }
+    POST /empresa/delete header{ "_id":"..." }
+    PUT /empresa/update body{ "_id":"...", "nome":"...", "cnpj":"...", "telefone":"...", "tipoPgto":"..." }
 
 ::::PACIENTES::::::::
-    /paciente/new body{ "nome":"...", "cpf":"...", "codEmpresa":"...", "codFuncao":"...", "codSubgrupo":"..." }
-    /paciente/read header{ "_id":"..." }
-    /paciente/delete header{ "_id":"..." }
-    /paciente/update body{ "_id":"...", "nome":"...", "cpf":"...", "codEmpresa":"...", "codFuncao":"...", "codSubgrupo":"..." }
+    POST /paciente/new body{ "nome":"...", "cpf":"...", "codEmpresa":"...", "codFuncao":"...", "codSubgrupo":"..." }
+    GET /paciente/read header{ "_id":"..." }
+    POST /paciente/delete header{ "_id":"..." }
+    PUT /paciente/update body{ "_id":"...", "nome":"...", "cpf":"...", "codEmpresa":"...", "codFuncao":"...", "codSubgrupo":"..." }
 
 ::::FUNÇÕES::::::::::
-    /funcao/new body{ "nome":"...", "descricao":"...", "exames": ["...", "...", "...", ...] }
-    /funcao/read header{ "_id":"..." }
-    /funcao/delete header{ "_id":"..." }
-    /funcao/update body{ "_id":"...", "nome":"...", "descricao":"...", "exames": ["...", "...", "...", ...] }
+    POST /funcao/new body{ "nome":"...", "descricao":"...", "exames": ["...", "...", "...", ...] }
+    GET /funcao/read header{ "_id":"..." }
+    POST /funcao/delete header{ "_id":"..." }
+    PUT /funcao/update body{ "_id":"...", "nome":"...", "descricao":"...", "exames": ["...", "...", "...", ...] }
 
 ::::ATIVIDADE::::::::
-    /atividade/new body{ "nome":"...", "descricao":"..." }
-    /atividade/read header{ "_id":"..." }
-    /atividade/delete header{ "_id":"..." }
-    /atividade/update body{ "_id":"...", "nome":"...", "descricao":"..." }
+    POST /atividade/new body{ "nome":"...", "descricao":"..." }
+    GET /atividade/read header{ "_id":"..." }
+    POST /atividade/delete header{ "_id":"..." }
+    PUT /atividade/update body{ "_id":"...", "nome":"...", "descricao":"..." }
 
 ::::SUBGRUPO:::::::::
-    /subgrupo/new body{ "nome":"...", "codFuncao":"...", "atividades": ["...", "...", "...", ...] }
-    /subgrupo/read header{ "_id":"..." }
-    /subgrupo/delete header{ "_id":"..." }
-    /subgrupo/update body{ "_id":"...", "nome":"...", "codFuncao":"...", "atividades": ["...", "...", "...", ...] }
+    POST /subgrupo/new body{ "nome":"...", "codFuncao":"...", "atividades": ["...", "...", "...", ...] }
+    GET /subgrupo/read header{ "_id":"..." }
+    POST /subgrupo/delete header{ "_id":"..." }
+    PUT /subgrupo/update body{ "_id":"...", "nome":"...", "codFuncao":"...", "atividades": ["...", "...", "...", ...] }
 
 ::::MÉDICO:::::::::::
-    /especialidade/new body{ "nome":"...", "descricao":"..." }
-    /especialidade/read header{ "_id":"..." }
-    /especialidade/delete header{ "_id":"..." }
-    /especialidade/update body{ "_id":"...", "nome":"...", "descricao":"..." }
+    POST /especialidade/new body{ "nome":"...", "descricao":"..." }
+    GET /especialidade/read header{ "_id":"..." }
+    POST /especialidade/delete header{ "_id":"..." }
+    PUT /especialidade/update body{ "_id":"...", "nome":"...", "descricao":"..." }
 
-    /medico/new body{ "nome":"...", "cpf":"...", "crm":"...", "especialidades": ["...", "...", "...", ...] }
-    /medico/read header{ "_id":"..." }
-    /medico/delete header{ "_id":"..." }
-    /medico/update body{ "_id":"...", "nome":"...", "cpf":"...", "crm":"...", "especialidades": ["...", "...", "...", ...] }
+    POST /medico/new body{ "nome":"...", "cpf":"...", "crm":"...", "especialidades": ["...", "...", "...", ...] }
+    GET /medico/read header{ "_id":"..." }
+    POST /medico/delete header{ "_id":"..." }
+    PUT /medico/update body{ "_id":"...", "nome":"...", "cpf":"...", "crm":"...", "especialidades": ["...", "...", "...", ...] }
 
 ::::EXAME::::::::::::
-    /exame/new body{ "nome":"...", "descricao":"...", "preco":"..." }
-    /exame/read header{ "_id":"..." }
-    /exame/delete header{ "_id":"..." }
-    /exame/update body{ "_id":"...", "nome":"...", "descricao":"...", "preco":"..." }
+    POST /exame/new body{ "nome":"...", "descricao":"...", "preco":"..." }
+    GET /exame/read header{ "_id":"..." }
+    POST /exame/delete header{ "_id":"..." }
+    PUT /exame/update body{ "_id":"...", "nome":"...", "descricao":"...", "preco":"..." }
 
 */
