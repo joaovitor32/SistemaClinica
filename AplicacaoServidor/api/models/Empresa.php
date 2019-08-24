88cb3c2fab64bfc196c9e775bce23fbb92c9c52a
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-08-24 11:53:08 -0300

    Atualização do modelo, incluindo funções de CRUD.

--- AplicacaoServidor/api/models/Empresa.php
+++ AplicacaoServidor/api/models/Empresa.php
@@ -75,7 +75,106 @@
             //Autorizar CORS para testes antes do controller
             header("Access-Control-Allow-Origin: *");
             echo json_encode($this->lista());
-        }   
+        }
+        public function create(){
+            
+            try {
+
+                include('../database.class.php');
+
+                $db = new database();
+                $db->setUsuario($this->dbUsuario);
+                $db->setSenha($this->dbSenha);
+
+                $conexao = $db->conecta_mysql();
+
+                $sqlCreate = "INSERT INTO empresa(nome,cnpj,telefone,tipoPgto)  VALUES (?,?,?,?)";
+                $conexao->exec('SET NAMES utf8');
+                $stmtCreate = $conexao->prepare($sqlCreate);
+                $stmtCreate->bindParam(1,$this->nome);
+                $stmtCreate->bindParam(2,$this->cnpj);
+                $stmtCreate->bindParam(3,$this->telefone);
+                $stmtCreate->bindParam(4,$this->tipoPgto);
+                echo($stmtCreate->execute());
+
+            } catch (PDOExcpetion $e) {
+                echo "Erro: ".$e->getMessage();
+            }
+        }
+        public function read(){
+            
+            try {
+
+                include('../database.class.php');
+
+                $db = new database();
+                $db->setUsuario($this->dbUsuario);
+                $db->setSenha($this->dbSenha);
+
+                $conexao = $db->conecta_mysql();
+
+                $sqlRead = "SELECT *  FROM empresa WHERE codEmpresa = ?";
+                $conexao->exec('SET NAMES utf8');
+                $stmtRead = $conexao->prepare($sqlRead);
+                $stmtRead->bindParam(1,$this->codEmpresa);
+                $stmtRead->execute();
+
+                $empresa = $stmtRead->fetch(PDO::FETCH_ASSOC);
+
+                echo json_encode($empresa);
+
+            } catch (PDOExcpetion $e) {
+                echo "Erro: ".$e->getMessage();
+            }
+        }
+        public function update(){
+            
+            try {
+
+                include('../database.class.php');
+
+                $db = new database();
+                $db->setUsuario($this->dbUsuario);
+                $db->setSenha($this->dbSenha);
+
+                $conexao = $db->conecta_mysql();
+
+                $sqlUpdate = "UPDATE empresa SET nome = ?, cnpj = ?, telefone = ?, tipoPgto = ? WHERE codEmpresa = ?";
+                $conexao->exec('SET NAMES utf8');
+                $stmtUpdate = $conexao->prepare($sqlUpdate);
+                $stmtUpdate->bindParam(1,$this->nome);
+                $stmtUpdate->bindParam(2,$this->cnpj);
+                $stmtUpdate->bindParam(3,$this->telefone);
+                $stmtUpdate->bindParam(4,$this->tipoPgto);
+                $stmtUpdate->bindParam(5,$this->codEmpresa);
+                echo($stmtUpdate->execute());
+
+            } catch (PDOExcpetion $e) {
+                echo "Erro: ".$e->getMessage();
+            }
+        }
+        public function delete(){
+            
+            try {
+
+                include('../database.class.php');
+
+                $db = new database();
+                $db->setUsuario($this->dbUsuario);
+                $db->setSenha($this->dbSenha);
+
+                $conexao = $db->conecta_mysql();
+
+                $sqlDelete = "DELETE FROM empresa WHERE codEmpresa = ?";
+                $conexao->exec('SET NAMES utf8');
+                $stmtDelete = $conexao->prepare($sqlDelete);
+                $stmtDelete->bindParam(1,$this->codEmpresa);
+                echo($stmtDelete->execute());
+
+            } catch (PDOExcpetion $e) {
+                echo "Erro: ".$e->getMessage();
+            }
+        }
     
     }
 
