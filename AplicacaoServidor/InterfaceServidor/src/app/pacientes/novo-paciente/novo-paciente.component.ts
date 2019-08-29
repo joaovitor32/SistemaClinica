6c184eafa5ddb8afd2ebddd199e10ecc159d6412
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-08-29 18:59:13 -0300

    Atualização das telas de cadastro de Pacientes.

--- AplicacaoServidor/InterfaceServidor/src/app/pacientes/novo-paciente/novo-paciente.component.ts
+++ AplicacaoServidor/InterfaceServidor/src/app/pacientes/novo-paciente/novo-paciente.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SidenavComponent } from '../../sidenav/sidenav.component';
 
 @Component({
-  selector: 'app-novo-paciente',
-  templateUrl: './novo-paciente.component.html',
-  styleUrls: ['./novo-paciente.component.css']
+	selector: 'app-novo-paciente',
+	templateUrl: './novo-paciente.component.html',
+	styleUrls: ['./novo-paciente.component.css']
 })
 export class NovoPacienteComponent implements OnInit {
 
-  constructor() { }
+	constructor(public sideNav:SidenavComponent) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.sideNav.activeView = "Pacientes > Novo Paciente";
+	}
 
 }
