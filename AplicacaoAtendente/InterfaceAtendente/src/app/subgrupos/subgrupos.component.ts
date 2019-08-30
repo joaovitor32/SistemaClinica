773e69409cdab8f9dc7f44d602b920e98ba57c31
Arma-X <hugob.rangel@gmail.com>
2019-08-30 07:28:12 -0300

    mudanças na interface

---
+++ AplicacaoAtendente/InterfaceAtendente/src/app/subgrupos/subgrupos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subgrupos',
+  templateUrl: './subgrupos.component.html',
+  styleUrls: ['./subgrupos.component.css']
+})
+export class SubgruposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
