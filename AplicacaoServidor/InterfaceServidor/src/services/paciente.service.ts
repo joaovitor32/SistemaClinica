1f1ce0caed2b24c07255c3e256d8055f39c97a7f
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-08-29 21:47:06 -0300

    Adaptações com criação do Service Paciente.

---
+++ AplicacaoServidor/InterfaceServidor/src/services/paciente.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PacienteService {
+
+	constructor() { }
+
+	async listaDePacientes(){
+		let pacientes;
+		// await fetch('http://localhost:8080/Paciente.php')
+		await fetch('http://localhost/SistemaClinica/AplicacaoServidor/api/models/Empresa.php')
+		.then(blob => blob.json())
+		.then(data => pacientes = data);
+		return pacientes;
+	}
+}
