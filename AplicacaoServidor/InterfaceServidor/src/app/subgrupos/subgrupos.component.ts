715ca0d331991f399482bb2c597ac01089b48106
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-08-29 21:46:46 -0300

    Adaptações com criação do Service Subgrupo.

--- AplicacaoServidor/InterfaceServidor/src/app/subgrupos/subgrupos.component.ts
+++ AplicacaoServidor/InterfaceServidor/src/app/subgrupos/subgrupos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SidenavComponent } from '../sidenav/sidenav.component';
+import { SubgrupoService } from '../../services/subgrupo.service';
 
 @Component({
 	selector: 'app-subgrupos',
@@ -8,10 +9,39 @@ import { SidenavComponent } from '../sidenav/sidenav.component';
 })
 export class SubgruposComponent implements OnInit {
 
-	constructor(public sideNav:SidenavComponent) { }
+	subgrupos:any;
+	html:string;
 
-	ngOnInit() {
+	constructor(public sideNav:SidenavComponent, private subgrupoService:SubgrupoService) { }
+
+	async ngOnInit() {
 		this.sideNav.activeView="Subgrupos";
+		this.subgrupos = await this.subgrupoService.listaDeSubgrupo();
+		this.html = this.popularTabela();
 	}
 
+	popularTabela(){
+		let i = 0;
+		var html = this.subgrupos.map( subgrupo => {
+			return '<tr data-codigo='+ subgrupo.codSubgrupo +'><th scope="row"> '+ (++i) +' </th><td> '+ subgrupo.nome +' </td><td> '+ subgrupo.codFuncao +' </td></tr>';
+		}).join('');
+		
+		return html;
+	}
+
+	buscaTermo(event){
+		const regex = new RegExp(event.target.value, 'gi');
+		
+		var busca = this.subgrupos.filter( subgrupo => {
+			return subgrupo.nome.match(regex) || subgrupo.codFuncao.match(regex);
+		});
+		this.mostraBusca(busca);
+	}
+
+	mostraBusca(resultadoBusca){
+		let i =0;
+		this.html = resultadoBusca.map( subgrupo => {
+			return '<tr data-codigo='+ subgrupo.codSubgrupo +'><th scope="row"> '+ (++i) +' </th><td> '+ subgrupo.nome +' </td><td> '+ subgrupo.codFuncao +' </td></tr>';
+		}).join('');
+	}
 }
