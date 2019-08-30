da1b9e178214a75d5e2487cf4895608ff134331a
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-08-29 21:47:43 -0300

    Adaptações com criação do Service Exames.

---
+++ AplicacaoServidor/InterfaceServidor/src/services/exame.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExameService {
+
+	constructor() { }
+
+	async listaDeExames(){
+		let exames;
+		// await fetch('http://localhost:8080/Exame.php')
+		await fetch('http://localhost/SistemaClinica/AplicacaoServidor/api/models/Empresa.php')
+		.then(blob => blob.json())
+		.then(data => exames = data);
+		return exames;
+	}
+}
