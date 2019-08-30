818014f4fd427c8c31a04473a1c70bc2e45f1008
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-08-29 21:47:32 -0300

    Adaptações com criação do Service Funções.

---
+++ AplicacaoServidor/InterfaceServidor/src/services/funcao.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncaoService {
+
+	constructor() { }
+
+	async listaDeFuncoes(){
+		let funcoes;
+		// await fetch('http://localhost:8080/Funcao.php')
+		await fetch('http://localhost/SistemaClinica/AplicacaoServidor/api/models/Empresa.php')
+		.then(blob => blob.json())
+		.then(data => funcoes = data);
+		return funcoes;
+	}
+}
