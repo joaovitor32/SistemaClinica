66996ee74bba257cfbd526aba31b5638b100aa05
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-11-08 08:04:07 -0200

    Preparação para adicionar CRUDs.

---
+++ AplicacaoServidor/InterfaceServidor/src/app/medicos/modal-medicos/modal-medicos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-medicos',
+  templateUrl: './modal-medicos.component.html'
+})
+export class ModalMedicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ AplicacaoServidor/InterfaceServidor/src/app/subgrupos/modal-subgrupos/modal-subgrupos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-subgrupos',
+  templateUrl: './modal-subgrupos.component.html'
+})
+export class ModalSubgruposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
