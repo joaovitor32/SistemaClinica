416f3e9ce235c0d24a7284d2a823b426202c2e18
Arma-X <hugob.rangel@gmail.com>
2020-07-07 20:08:30 -0300

    Angular/Electron .Codigo comentado

--- AplicacaoAtendente/InterfaceAtendente/src/app/services/header.interceptor.ts
+++ AplicacaoAtendente/InterfaceAtendente/src/app/services/header.interceptor.ts
@@ -7,10 +7,14 @@ import { Observable } from 'rxjs';
 })
 export class Interceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+        //-------------------------- Descomente abaixo para rodar no *ELECTRON* ------------------------//
         const user = localStorage.getItem("user");
-        const password = localStorage.getItem("password");
-
+        const password = localStorage.getItem("password"); 
+        //-------------------------- Descomente abaixo para rodar no *ANGULAR* ------------------------//
+        // const user = "servidorLabmed" ;
+        // const password = "labmed2019" ; 
+        // *Lembre de trocar o caminho do url nos services* //
+        //-------------------------- daqui pra baixo não é necessário mudar------------------------//
         const newRequest = request.clone({
             headers: request.headers
                 .set('db_user', user)
